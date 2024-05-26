21bc6b44d61c4af0c0781d0db54916a3b2d2e5a2
Lyvmi <jsolanoalarcos@gmail.com>
2024-05-26 17:14:17 +0200

    sidebar nav added

--- src/renderer.js
+++ src/renderer.js
@@ -1,1 +1,23 @@
-console.log("Renderer working")
+const body = document.querySelector("body"),
+        sidebar = body.querySelector(".sidebar"),
+        toggle = body.querySelector(".toggle");
+
+        
+        toggle.addEventListener("click", () =>{
+            if (sidebar.classList.contains("close")){
+                sidebar.classList.remove("close");
+                sidebar.classList.add("open");
+            }
+            else{
+                sidebar.classList.remove("open");
+                sidebar.classList.add("close");
+            }
+            if (toggle.classList.contains("bx-chevron-right")){
+                toggle.classList.remove("bx-chevron-right");
+                toggle.classList.add("bx-chevron-left");
+            }
+            else{
+                toggle.classList.remove("bx-chevron-left");
+                toggle.classList.add("bx-chevron-right");
+            }
+        })
