18047cd79b08531ead6dda60b45306a9b9aac2c9
Lyvmi <jsolanoalarcos@gmail.com>
2024-05-29 10:18:41 +0200

    Note container added

--- src/renderer.js
+++ src/renderer.js
@@ -1,14 +1,15 @@
 const body = document.querySelector("body"),
         sidebar = body.querySelector(".sidebar"),
         toggle = body.querySelector(".toggle"),
-        notes = body.querySelector(".note"),
-        folder_container = body.querySelector(".folder-container");
+        folder_container = body.querySelector(".folder-container"),
+        note_container = body.querySelector(".note-container");
 
         
         toggle.addEventListener("click", () =>{
             sidebar.classList.toggle("close");
             folder_container.classList.toggle("close");
-            toggle.classList.toggle("close")
+            toggle.classList.toggle("close");
+            note_container.classList.toggle("close");
 
             if (toggle.classList.contains("bx-chevron-right")){
                 toggle.classList.remove("bx-chevron-right");
