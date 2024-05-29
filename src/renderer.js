76814a52ab5e05b717d60ccf6267c799bc6eb70b
Lyvmi <jsolanoalarcos@gmail.com>
2024-05-29 09:58:09 +0200

    Folder container hecho

--- src/renderer.js
+++ src/renderer.js
@@ -1,18 +1,15 @@
 const body = document.querySelector("body"),
         sidebar = body.querySelector(".sidebar"),
-        toggle = body.querySelector(".toggle");
-        notes = body.querySelector(".note")
+        toggle = body.querySelector(".toggle"),
+        notes = body.querySelector(".note"),
+        folder_container = body.querySelector(".folder-container");
 
         
         toggle.addEventListener("click", () =>{
-            if (sidebar.classList.contains("close")){
-                sidebar.classList.remove("close");
-                sidebar.classList.add("open");
-            }
-            else{
-                sidebar.classList.remove("open");
-                sidebar.classList.add("close");
-            }
+            sidebar.classList.toggle("close");
+            folder_container.classList.toggle("close");
+            toggle.classList.toggle("close")
+
             if (toggle.classList.contains("bx-chevron-right")){
                 toggle.classList.remove("bx-chevron-right");
                 toggle.classList.add("bx-chevron-left");
